638eca283ac6b80309d0c63493bd197d8ce9a3e6
kirankumar242018 <kirankumar242018@gmail.com>
2019-09-13 15:51:08 +0530

    track1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Radix-touch'));
+app.use(express.static(__dirname + '/dist/radixtouch'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Radix-touch/index.html'));
+res.sendFile(path.join(__dirname+'/dist/radixtouch/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
