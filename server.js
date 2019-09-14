2c65ce4e4ab66c953dcb23fea5b003150d78ea64
kirankumar242018 <kirankumar242018@gmail.com>
2019-09-14 11:56:50 +0530

    changes

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/dist/radixtouch'));
  
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/radixtouch/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/radixtouch/index.html'));
+// });
  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
